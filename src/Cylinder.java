2d3ce435797e1fb96165eebc10c167db03661dd1
ProCherk <bog.mak2002@gmail.com>
2022-02-03 18:54:39 +0200

    1 try

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
 
-public class Cylinder
-{
-    public static int Cylinder_V(int h, int r)
-    {
-        double a = (3.14*r*r)*h;
-        int V = (int)Math.round(a);
+public class Cylinder {
+    public static int Cylinder_V(int h, int r) {
+        double a = (3.14 * r * r) * h;
+        int V = (int) Math.round(a);
         return V;
     }
 
-    public static int Cylinder_S(int h, int r)
-    {
-        double a = 2*(3.14*r*r)+(2*3.14*r)*h;
-        int S = (int)Math.round(a);
+    public static int Cylinder_S(int h, int r) {
+        double a = 2 * (3.14 * r * r) + (2 * 3.14 * r) * h;
+        int S = (int) Math.round(a);
         return S;
     }
 
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Input height: ");
